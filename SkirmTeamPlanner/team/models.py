8c84aeab74dc2d65894e00ec0619e4eff1962faa
Remco <remcoij@gmail.com>
2016-07-11 21:01:41 +0200

    Added some model fields

--- SkirmTeamPlanner/team/models.py
+++ SkirmTeamPlanner/team/models.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Team(models.Model):
-    title = models.CharField(max_length=100)
-    team_members = models.ManyToManyField(TeamMember)
-
 class TeamMember(models.Model):
     member_status = models.CharField(max_length=100)
     user_profile = models.ForeignKey(User)
+
+class Team(models.Model):
+    title = models.CharField(max_length=100)
+    team_members = models.ManyToManyField(TeamMember)
+    team_logo = models.ImageField()
+    description = models.CharField(max_length=500)
 
