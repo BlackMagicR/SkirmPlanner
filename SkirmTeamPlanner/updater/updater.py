c993a68575fa21ac679a6dafe903a399dd26085f
BlackMagic <remcoij@gmail.com>
2016-07-18 22:34:10 +0200

    Finally able to get the title for the events in the updater

--- SkirmTeamPlanner/updater/updater.py
+++ SkirmTeamPlanner/updater/updater.py
@@ -7,14 +7,12 @@ def main():
     #Here we deploy all threads that we use to scrape the html in a quick fashion
     page_counter = 1
     failed_url_list = []
-    while page_counter < 10:
+    while page_counter < 2:
         url = "http://www.nabv.nl/evenementen/page/%d" % page_counter
         print url
         try:
             page = requests.get(url)
             tree = html.fromstring(page.content)
-            print tree
-            print "Starting thread"
             thread = Thread(target=parse_skirms, args=(tree,))
             thread.start()
             thread.join()
@@ -27,8 +25,13 @@ def main():
 
 
 def parse_skirms(html):
-    print "Start parsing the html"
-    print html.xpath('//*[@id="ctl00_sectionContent_ctl10_ctl00_ctl00_dynamicContentListView_ctrl0_Label1"]/text()')
+    print "----------------Start parsing the html-----------------"
+    elements = html.xpath('//h2[@class="sfitemTitle"]/span[starts-with(@id, "ctl00_sectionContent_ctl10_ctl00_ctl00_dynamicContentListView_ctrl")]/text()')
+    # elements = html.xpath('//*[@id="ctl00_sectionContent_ctl10_ctl00_ctl00_dynamicContentListView_ctrl"]')
+    for element in elements:
+        print element.encode('UTF-8')
+        
+    
 
 if __name__ == "__main__":
     main()
