18f9c143556e8cd5e1c98fc09ab0df59a1f40acf
BlackMagic <remcoij@gmail.com>
2016-07-26 14:32:11 +0200

    Added a few new models for the planner

--- SkirmTeamPlanner/team/models.py
+++ SkirmTeamPlanner/team/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from planner.models import PlannedEvent
+from updater.models import Skirm
+
 # Create your models here.
 
 class TeamMember(models.Model):
@@ -12,4 +15,7 @@ class Team(models.Model):
     team_members = models.ManyToManyField(TeamMember)
     team_logo = models.ImageField()
     description = models.CharField(max_length=500)
+    #Doing the following will allow us to add more information to the skirm using the relationship. For instance:
+    #You want to add the members joining this event to a model in the database but it makes no sense to add them to the Skirm model since it is unique to every PlannedEvent.
+    planned_skirms = models.ManyToManyField(Skirm, through='PlannedEvent')
 
