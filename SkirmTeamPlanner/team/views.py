781910f23d7efc1fd5773ec5911ca83fdecb95fd
BlackMagic <remcoij@gmail.com>
2016-08-18 22:06:17 +0200

    Forgot to save this file, lets try that again

--- SkirmTeamPlanner/team/views.py
+++ SkirmTeamPlanner/team/views.py
@@ -5,4 +5,4 @@ class TeamPage(TemplateView):
     template_name = "team.tmpl"
 
     def get_context_data(self, **kwargs):
-        #Is user part of a team? Yes: load team info/ No: load 50/50 join or create team. Perhaps include a search box
+        #Is user part of a team? Yes: load team info/ No: load 50/50 page search and join or create team.
